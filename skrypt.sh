ae3e9541ec0b3a4a7720ba2591cd4bddc014785b
dsw49527 <dsw49527@student.dsw.edu.pl>
2023-06-28 16:06:26 +0200

    added shortcuts to options

--- skrypt.sh
+++ skrypt.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
-if [[ "$1" == "--date" ]]; then
-	date
-fi
+if [[ "$1" == "--date" || "$1" == "-d" ]]; then
+  date
+elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
+  if [[ -n "$2" ]]; then
+    num_files=$2
+  else
+    num_files=100
+  fi
 
-if [[ "$1" == "--logs" ]]; then
-  	if [ -n "$2"]; then
-    		num_files=$2
-	else
-		num_files=100
-	fi
-	
-	for ((i=1; i<=num_files; i++)); do
-		filename="log${i}.txt"
-      		echo "Nazwa pliku: $filename" >> "$filename"
-      		echo "Nazwa skryptu: skrypt.sh" >> "$filename"
-      		echo "Data: $(date)" >> "$filename"
-	done
-fi
-
-if [[ "$1" == "--help" ]]; then
-  	echo "Dostępne opcje:"
-  	echo "--date: Wyświetla dzisiejszą datę."
-  	echo "--logs <liczba_plików>: Tworzy określoną liczbę plików logx.txt, zawierających nazwę pliku, nazwę skryptu i datę."
+  for ((i=1; i<=num_files; i++)); do
+    filename="log${i}.txt"
+    echo "Nazwa pliku: $filename" >> "$filename"
+    echo "Nazwa skryptu: skrypt.sh" >> "$filename"
+    echo "Data: $(date)" >> "$filename"
+  done
+elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
+  echo "Dostępne opcje:"
+  echo "--date, -d: Wyświetla dzisiejszą datę."
+  echo "--logs, -l <liczba_plików>: Tworzy określoną liczbę plików logx.txt, zawierających nazwę pliku, nazwę skryptu i datę."
 else
-  	echo "Nieznana opcja. Użyj 'skrypt.sh --help' aby wyświetlić dostępne opcje."
+  echo "Nieznana opcja. Użyj 'skrypt.sh -h' aby wyświetlić dostępne opcje."
 fi
