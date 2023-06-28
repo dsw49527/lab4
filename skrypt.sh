3f1e97ffbf0d4910d63bab021a3341057c3b159b
dsw49527 <dsw49527@student.dsw.edu.pl>
2023-06-28 15:38:38 +0200

    added creating logs

--- skrypt.sh
+++ skrypt.sh
@@ -2,4 +2,12 @@
 
 if [[ "$1" == "--date" ]]; then
   echo "Dzisiejsza data: $(date)"
+elif [[ "$1" == "--logs" ]]; then
+  for ((i=1; i<=100; i++))
+  do
+    filename="log${i}.txt"
+    echo "Nazwa pliku: $filename" >> "$filename"
+    echo "Nazwa skryptu: skrypt.sh" >> "$filename"
+    echo "Data: $(date)" >> "$filename"
+  done
 fi
