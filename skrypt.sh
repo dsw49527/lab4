3b6741f0ae9848f12e140661385e0ffd498b11e6
dsw49527 <dsw49527@student.dsw.edu.pl>
2023-06-28 16:18:51 +0200

    added git clone function

--- skrypt.sh
+++ skrypt.sh
@@ -15,9 +15,15 @@ elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
     echo "Nazwa skryptu: skrypt.sh" >> "$filename"
     echo "Data: $(date)" >> "$filename"
   done
+elif [["$1" == "--init" || "$1" == "-i"]]; then
+	git clone https://github.com/dsw49527/lab4
+	export PATH"$PWD:$PATH"
+	echo "Repozytorium zostało sklonowane do bieżącego katalogu"
+	echo "Ścieżka do repozytorium dodała dodana do zmiennej środowiskowej PATH"
 elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
   echo "Dostępne opcje:"
   echo "--date, -d: Wyświetla dzisiejszą datę."
+  echo "--init, -i: Clonuje repozytorium i dodaje zmienną środowiskową PATH"
   echo "--logs, -l <liczba_plików>: Tworzy określoną liczbę plików logx.txt, zawierających nazwę pliku, nazwę skryptu i datę."
 else
   echo "Nieznana opcja. Użyj 'skrypt.sh -h' aby wyświetlić dostępne opcje."
