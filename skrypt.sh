9fce313e0acfd37a69f7b55687d23789d84accf7
dsw49527 <dsw49527@student.dsw.edu.pl>
2023-06-28 16:27:29 +0200

    added creating errors

--- skrypt.sh
+++ skrypt.sh
@@ -2,6 +2,7 @@
 
 if [[ "$1" == "--date" || "$1" == "-d" ]]; then
   date
+
 elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
   if [[ -n "$2" ]]; then
     num_files=$2
@@ -15,16 +16,35 @@ elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
     echo "Nazwa skryptu: skrypt.sh" >> "$filename"
     echo "Data: $(date)" >> "$filename"
   done
+
+elif [ "$1" = "--error" ] || [ "$1" = "-e" ]; then
+    if [ -n "$2" ]; then
+        num_files="$2"
+    else
+        num_files=100
+    fi
+    
+    for ((i=1; i<=num_files; i++)); do
+        dirname="error${i}"
+	mkdir -p "$dirname"
+	filename="${dirname}/error${i}.txt
+        echo "Nazwa pliku: $filename" > "$filename"
+        echo "Nazwa skryptu: skrypt.sh" >> "$filename"
+        echo "Data: $(date)" >> "$filename"
+    done
+
 elif [["$1" == "--init" || "$1" == "-i"]]; then
 	git clone https://github.com/dsw49527/lab4
 	export PATH"$PWD:$PATH"
 	echo "Repozytorium zostało sklonowane do bieżącego katalogu"
 	echo "Ścieżka do repozytorium dodała dodana do zmiennej środowiskowej PATH"
+
 elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
   echo "Dostępne opcje:"
   echo "--date, -d: Wyświetla dzisiejszą datę."
   echo "--init, -i: Clonuje repozytorium i dodaje zmienną środowiskową PATH"
-  echo "--logs, -l <liczba_plików>: Tworzy określoną liczbę plików logx.txt, zawierających nazwę pliku, nazwę skryptu i datę."
+  echo "--logs, -l <liczba_plików>: Tworzy określoną liczbę plików logx.txt."
+  echo "--error, -e <liczba_plików: Tworzy określoną liczbę plików errorx.txt."
 else
   echo "Nieznana opcja. Użyj 'skrypt.sh -h' aby wyświetlić dostępne opcje."
 fi
